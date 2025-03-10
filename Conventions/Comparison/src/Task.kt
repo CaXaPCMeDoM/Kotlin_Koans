964c9a2bd3b27ed0e8df89d0289c1171d7f52613
CaXaPCMeDoM <ZGHTLGHBYBVFNTKM@mail.ru>
2025-03-10 11:51:43 +0300

    Initial commit

---
+++ Conventions/Comparison/src/Task.kt
@@ -0,0 +1,10 @@
+data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int) : Comparable<MyDate> {
+    override fun compareTo(other: MyDate): Int {
+        return compareValuesBy(this, other, MyDate::year, MyDate::month, MyDate::dayOfMonth)
+    }
+
+}
+
+fun test(date1: MyDate, date2: MyDate) {
+    println(date1 < date2)
+}
---
+++ Properties/Lazy property/src/Task.kt
@@ -0,0 +1,13 @@
+class LazyProperty(val initializer: () -> Int) {
+    private var initialized = false
+    private var value: Int? = null
+
+    val lazy: Int
+        get() {
+            if (!initialized) {
+                value = initializer()
+                initialized = true
+            }
+            return value!!
+        }
+}
